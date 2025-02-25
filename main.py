68af6c79b1ad8d185066aed237be22be08748b3f
reissko <reissoliveros@gmail.com>
2025-02-24 22:43:12 -1000

    Working input -> output RAG flow

--- main.py
+++ main.py
@@ -1,116 +1,80 @@
 # Langchain dependencies
-from langchain_community.document_loaders import PyPDFDirectoryLoader # Importing PDF loader from Langchain
-from langchain.text_splitter import RecursiveCharacterTextSplitter # Importing text splitter from Langchain
-from langchain_community.embeddings import OpenAIEmbeddings # Importing OpenAI embeddings from Langchain
-from langchain.schema import Document # Importing Document schema from Langchain
-from langchain_community.vectorstores import Chroma # Importing Chroma vector store from Langchain
-from dotenv import load_dotenv # Importing dotenv to get API key from .env file
-from langchain_community.chat_models import ChatOpenAI # Import OpenAI LLM
-from langchain_openai import OpenAIEmbeddings # Import OpenAI LLM
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import PyPDFDirectoryLoader 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_ollama import ChatOllama
 
-import os # Importing os module for operating system functionalities
-import shutil # Importing shutil module for high-level file operations
 
-# path to data
+
 DATA_PATH = 'materials'
-# path to chroma DB
 CHROMA_PATH = 'chroma'
 
-query_text = "Explain what a linked list is and how it is used"
+query_text = "What are some in real life applications of stacks and queues?"
 
 PROMPT_TEMPLATE = """
-Answer the question based only on the following context:
+You are a tutor for a Data Structures class. Answer the user's question as concisely as possible;
+Preferably in 3-5 sentences.
+You are given the following context to use as sources of data:
 {context}
 
-Answer the question based on the above context: {question}
+Answer the following question based on the provided context:
+"{question}"
 """
 
+
 def load_documents():
-  # Initialize PDF loader with specified directory
-  document_loader = PyPDFDirectoryLoader(DATA_PATH) 
-  # Load PDF documents and return them as a list of Document objects
-  return document_loader.load() 
+  docs = PyPDFDirectoryLoader(DATA_PATH).load()
+  return docs
 
-def split_text(documents: list[Document]):
-  # Initialize text splitter with specified parameters
+def split_documents(docs):
   text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, # Size of each chunk in characters
-    chunk_overlap=150, # Overlap between consecutive chunks
-    length_function=len, # Function to compute the length of the text
-    add_start_index=True, # Flag to add start index to each chunk
+    chunk_size=1000,
+    chunk_overlap=150,
+    length_function=len
   )
+  return text_splitter.split_documents(docs)
 
-  # Split documents into smaller chunks using text splitter
-  chunks = text_splitter.split_documents(documents)
-  # print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
-
-  # Print example of page content and metadata for a chunk
-  document = chunks[0]
-  print(document.page_content)
-  print(document.metadata)
-
-  return chunks # Return the list of split text chunks
-
-def save_to_chroma(chunks: list[Document]):
-  # Clear out the existing database directory if it exists
+def create_vectorstore(chunks):
+  embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+  # if the chroma database exists already then delete it before creating a new one
+  """ 
   if os.path.exists(CHROMA_PATH):
     shutil.rmtree(CHROMA_PATH)
-
-  # Create a new Chroma database from the documents using OpenAI embeddings
-  db = Chroma.from_documents(
-    chunks,
-    OpenAIEmbeddings(model="text-embedding-3-large"),
+  """
+  vectorstore = Chroma.from_documents(
+    collection_name="cs305",
+    documents=chunks,
+    embedding=embeddings,
     persist_directory=CHROMA_PATH
   )
+  return vectorstore
 
-  # Persist the database to disk
-  db.persist()
-  print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
-
-def generate_data_store():
-  documents = load_documents() # Load documents from a source
-  chunks = split_text(documents) # Split documents into manageable chunks
-  save_to_chroma(chunks) # Save the processed data to a data store
-
-def query_rag(query_text):
-  # YOU MUST - Use same embedding function as before
-  embedding_function = OpenAIEmbeddings(model="text-embedding-3-large")
-
-  # Prepare the database
-  db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+def query_vectorstore(vectorstore, query_text):
+  query_vector = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2").embed_query(query_text)
+  docs = vectorstore.similarity_search_by_vector(query_vector, k=4)
+  # return the first (most similar) chunk
+  return docs
   
-  # Retrieving the context from the DB using similarity search
-  results = db.similarity_search_with_relevance_scores(query_text, k=3)
-
-  # Check if there are any matching results or if the relevance score is too low
-  if len(results) == 0 or results[0][1] < 0.7:
-    print(f"Unable to find matching results.")
-
-  # Combine context from matching documents
-  context_text = "\n\n - -\n\n".join([doc.page_content for doc, _score in results])
- 
-  # Create prompt template using context and query text
-  prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-  prompt = prompt_template.format(context=context_text, question=query_text)
+try:
+  # load the textbook PDF and chunk it
+  chunks = split_documents(load_documents())
+  # initialize the chromadb vectorstore with the embedded chunks
+  vector_store = create_vectorstore(chunks)
+  # context is an array of chunks (documents) to be used as context for the LLM
+  context = query_vectorstore(vector_store, query_text)
+  # create the prompt template
+  prompt_template = PROMPT_TEMPLATE.format(context=context, question=query_text)
   
-  # Initialize OpenAI chat model
-  model = ChatOpenAI()
+  # create the LLM
+  llm = ChatOllama(
+    model="llama3.2",
+    temperature=0,
+    prompt_template=prompt_template,
+  )
 
-  # Generate response text based on the prompt
-  response_text = model.predict(prompt)
- 
-   # Get sources of the matching documents
-  sources = [doc.metadata.get("source", None) for doc, _score in results]
- 
-  # Format and return response including generated text and sources
-  formatted_response = f"Response: {response_text}\nSources: {sources}"
-  return formatted_response, response_text
-
-load_dotenv()
-
-generate_data_store()
-
-formatted_response, response_text = query_rag(query_text)
-
-print(response_text)
+  # pass the question and context to the LLM
+  response = llm.invoke(input=prompt_template)
+  print(response.content)
+except Exception as e:
+  print(e)
